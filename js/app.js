30811bba8d2f38596ac0b04f847fd98a896d6ebc
ajmasnyj <36415269+ajmasnyj@users.noreply.github.com>
2018-07-16 15:24:16 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,51 +6,52 @@ var url = "http://jsr-twitter.herokuapp.com/";
 
 // create global array
 var arrayTweets = [];
-
+var until = "until=2018-07-15";
+var queryString = "q=%23scottiedog&count=5&"+until;
 // wrap get function in for loop for 7 days until=2018-xx-xx 7 times
 // for loop 7 days of tweets
-for (j = 9; j < 16; j++){
-  var until = "until=2018-07-"+j;
-  var queryString = "q=%23scottiedog&count=5&"+until;
-  // WorldCup2018
-  // baseball
-  // wimbledon
-
-  $.get( access + url , queryString, function(data){
-    // console.log(data);
-    addTweetsDOM(data);
-  });
-
-  function addTweetsDOM(data){
-    var searchTweets = data.statuses;
-    // console.log(searchTweets);
-    var numberTweets = searchTweets.length;
-
-    for (var i = 0; i < searchTweets.length; i++){
-      var dateTweets = searchTweets[i].created_at;
-      // console.log(tweetsDate);
-// concatenate 7 loops to global array
-      arrayTweets.push(dateTweets);
-      // console.log(arrayTweets);
-
-  // normalize dates, time of day, ignore hour minute seconds
-      var d = new Date(dateTweets);
-      var curr_date = d.getDate();
-      var curr_month = d.getMonth() + 1; //Months are zero based
-      var curr_year = d.getFullYear();
-      console.log(curr_date + "-" + curr_month + "-" + curr_year);
-
-    }
-
-  }
-
-}
-
-console.log(arrayTweets);
+// for (j = 9; j < 16; j++){
+//   var until = "until=2018-07-"+j;
+//   var queryString = "q=%23scottiedog&count=5&"+until;
+//   // WorldCup2018
+//   // baseball
+//   // wimbledon
+//
+//   $.get( access + url , queryString, function(data){
+//     // console.log(data);
+//     addTweetsDOM(data);
+//   });
+//
+//   function addTweetsDOM(data){
+//     var searchTweets = data.statuses;
+//     // console.log(searchTweets);
+//     var numberTweets = searchTweets.length;
+//
+//     for (var i = 0; i < searchTweets.length; i++){
+//       var dateTweets = searchTweets[i].created_at;
+//       // console.log(tweetsDate);
+// // concatenate 7 loops to global array
+//       arrayTweets.push(dateTweets);
+//       // console.log(arrayTweets);
+//
+//   // normalize dates, time of day, ignore hour minute seconds
+//       var d = new Date(dateTweets);
+//       var curr_date = d.getDate();
+//       var curr_month = d.getMonth() + 1; //Months are zero based
+//       var curr_year = d.getFullYear();
+//       console.log(curr_date + "-" + curr_month + "-" + curr_year);
+//
+//     }
+//
+//   }
+//
+// }
+//
+// console.log(arrayTweets);
 
 // D3
 // key value pair in JSON date and number of times
-var data = arrayTweets;
+// var data = toJSONArray(arrayTweets);
 
 // set margin of SVG
 var margin = {top: 50, right: 50, bottom: 50, left: 50};
@@ -75,38 +76,44 @@ var x = d3.scaleBand()
 var y = d3.scaleLinear()
     .range([height, 0])
 
-d3.something(data, function(error, data) {
 
-  // format the data
-  data.forEach(function(d) {
-    d.PlacehoderDate = +d.PlaceholderDate;
-  });
-
-// create individual bars
-// doamin tells D3 the range of data to expect
-// x axis is mapping the dates
-// y axis is number of tweets, 0 to max
-  // scale the range of data in domain
-  x.domain(data.map(function(d) { return d.PlaceholderTweets; }));
-  y.domain([0, d3.max(data, function(d) { return d.PlaceholderDate; })]);
-
-// create the bars with SVG rectange elements
-// use the x and y positions using variables above
-// set width and height of bars
-  svg.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d) { return x(d.PlaceholderDate); })
-    .attr("width", x.bandwidth())
-    .attr("y", function(d) { return y(d.PlaceholderTweets); })
-    .attr("height", function(d) { return height - y(d.PlaceholderTweets); });
-
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-
+d3.request(access + url + "?" + queryString, function(error, response) {
+    // Now use response to do some d3 magic
+    console.log("this is D3", response, error);
 });
+
+// d3.something(data, function(error, data) {
+//
+//   // format the data
+//   data.forEach(function(d) {
+//     d.PlaceholderDate = +d.PlaceholderDate;
+//   });
+//
+// // create individual bars
+// // doamin tells D3 the range of data to expect
+// // x axis is mapping the dates
+// // y axis is number of tweets, 0 to max
+//   // scale the range of data in domain
+//   x.domain(data.map(function(d) { return d.PlaceholderTweets; }));
+//   y.domain([0, d3.max(data, function(d) { return d.PlaceholderDate; })]);
+//
+// // create the bars with SVG rectange elements
+// // use the x and y positions using variables above
+// // set width and height of bars
+//   svg.selectAll(".bar")
+//     .data(data)
+//     .enter().append("rect")
+//     .attr("class", "bar")
+//     .attr("x", function(d) { return x(d.PlaceholderDate); })
+//     .attr("width", x.bandwidth())
+//     .attr("y", function(d) { return y(d.PlaceholderTweets); })
+//     .attr("height", function(d) { return height - y(d.PlaceholderTweets); });
+//
+//   svg.append("g")
+//     .attr("transform", "translate(0," + height + ")")
+//     .call(d3.axisBottom(x));
+//
+// });
 
 // var chart = d3.select(".chart")
 //     .attr("width", width);
